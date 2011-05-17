9ab01755e702b614897105fcb5d19d27c173e1b4
Remi Melisson <rmelisson@gmail.com>
2011-05-17 16:00:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lopet::Application.config.secret_token = '610c7345d6707a11fd9e6d6ad697762bdca2843a172edf143d4479895c3ae1ae0c03cc5f9ecefd2abae19e37c7a2a7390cc1649fe1d491853cce66511d19ca91'
