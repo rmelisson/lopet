e1b959285b886ca4d5e982a4719a2faa2d65a159
rmelisson <rmelisson@gmail.com>
2012-01-03 15:59:04 +0100

    add wkhtmlyopdf binaries for heroku -- tmp

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,6 @@
 PDFKit.configure do |config|
-   config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+    config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+    config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production?  
 end
 
 
